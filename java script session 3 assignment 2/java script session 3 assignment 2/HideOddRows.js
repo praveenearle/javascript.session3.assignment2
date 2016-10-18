d00dae28e848a20bc905165b12104fb1a9dba89f
praveenearle <praveen.earle@gmail.com>
2016-10-18 10:01:48 +0530

    java script s3a1
    java script s3a1

---
+++ java script session 3 assignment 2/java script session 3 assignment 2/HideOddRows.js
@@ -0,0 +1,15 @@
+﻿function hideoddrows() {
+    var rows = document.getElementById('ganguly').getElementsByTagName('tr');
+    for (var x = 1; x <= rows.length; x++) {
+        if (x % 2 == 0) {
+
+        } else {
+            document.getElementById('tr'+x).setAttribute('style','display:none');
+
+
+        }
+
+
+
+    }
+}
